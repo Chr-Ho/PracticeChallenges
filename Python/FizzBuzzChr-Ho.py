cb991d5bf2cb44cba78b57d1f24b34ae989b381c
Christopher Hofeldt <100885105+Chr-Ho@users.noreply.github.com>
2024-12-29 17:34:48 -0600

    Update and rename FizzBuzz.py to FizzBuzzChr-Ho.py

--- Python/FizzBuzzChr-Ho.py
+++ Python/FizzBuzzChr-Ho.py
@@ -5,3 +5,13 @@
 # Each number that is a multiple of 5, replace the number with Buzz
 # Each number that is a multiple of 3 and 5, replace the number with FizzBuzz
 
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
