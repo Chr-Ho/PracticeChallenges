ec49574c021d3e9da71696489131e7d755444460
Christopher Hofeldt <github.walrus313@passinbox.com>
2024-12-29 17:12:04 -0600

    Create FizzBuzz.py
    Completed FizzBuzz Challenge

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,18 @@
+# FizzBuzz Challenge
+
+# Display in the console 1-100 with each number on a seperate line.
+# Each number that is a multiple of 3, replace the number with Fizz
+# Each number that is a multiple of 5, replace the number with Buzz
+# Each number that is a multiple of 3 and 5, replace the number with FizzBuzz
+
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    
