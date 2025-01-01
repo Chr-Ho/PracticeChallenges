b33b038e4df4752f861449ec2419691aee573c33
Christopher Hofeldt <github.walrus313@passinbox.com>
2024-12-31 20:13:19 -0600

    Create TwoSumChr-Ho.py

---
+++ Completed/Python/TwoSumChr-Ho.py
@@ -0,0 +1,15 @@
+# Two Sum Challenge
+# Chr-Ho
+# 12/31/24
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))
